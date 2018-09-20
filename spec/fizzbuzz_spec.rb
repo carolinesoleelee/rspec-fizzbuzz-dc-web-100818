5fffa628306fc0055d6f8d2ef72a5bc2db0ce22a
Solee Lee <carolinesoleelee@gmail.com>
2018-09-20 20:48:22 +0000

    automatically backed up by learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -25,15 +25,3 @@ describe "fizzBuzz" do
     expect(fizz_4).to eq(nil)
   end
 end
-
-def fizzbuzz(number)
-  if number % 3 == 0
-    return "Fizz"
-  elsif number % 5 == 0
-    return "Buzz"
-  elsif number % 3 == 0 && number % 5 == 0
-    return "FizzBuzz"
-  else number % 3 || 5 != 0
-    return  nil
-  end
-end
